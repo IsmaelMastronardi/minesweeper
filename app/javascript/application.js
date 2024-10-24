e1278970315d244ce1157721d830a1c7949840e8
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-24 20:59:00 -0300

    make game playable

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.querySelectorAll('.boardSquare').forEach(square => {
+  square.addEventListener('click', function() {
+    console.log('aaa')
+    const isRevealed = this.dataset.revealed === 'true';
+
+    if (!isRevealed) {
+      this.dataset.revealed = 'true';
+      console.log(this.dataset.bomb)
+      if(this.dataset.bomb === 'true'){
+        this.innerHTML = '<span>💣</span>';
+      }
+      else {
+        this.innerHTML = `<span>${this.dataset.attachedBombs}</span>`;
+      }
+
+    }
+  });
+});
