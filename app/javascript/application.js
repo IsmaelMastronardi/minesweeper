71f034e96f3aaa3fdc0907196d13563842e6f845
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-24 21:33:23 -0300

    add show and refactor functions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,31 +2,43 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-const squares = document.querySelectorAll('.boardSquare')
+
+const squares = document.querySelectorAll('.boardSquare');
+const resetBtn = document.getElementById('reset_btn');
+const showBtn = document.getElementById('show_btn');
+const game_over_display = document.getElementById('game_over')
+
+const changeDisplayedValue = (el) => {
+  if(el.dataset.bomb === 'true'){
+    el.innerHTML = '<span>💣</span>';
+  }
+  else {
+    el.innerHTML = `<span>${el.dataset.attachedBombs}</span>`;
+  }
+}
+
+const showGameOver = (el) => {
+  if(el.dataset.bomb === 'true'){
+    game_over_display.innerText = 'You Lost.'
+    game_over_display.classList.remove('hidden');
+    game_over_display.classList.add('alert-danger');
+  }
+}
+
 squares.forEach(square => {
-  const game_over_display = document.getElementById('game_over')
   let gameLost = false;
   square.addEventListener('click', function() {
     if (gameLost) return;
     const isRevealed = this.dataset.revealed === 'true';
     if (!isRevealed) {
       this.dataset.revealed = 'true';
-      if(this.dataset.bomb === 'true'){
-        this.innerHTML = '<span>💣</span>';
-        game_over_display.innerText = 'You Lost.'
-        game_over_display.classList.remove('hidden');
-        game_over_display.classList.add('alert-danger')
-      }
-      else {
-        this.innerHTML = `<span>${this.dataset.attachedBombs}</span>`;
-      }
+      changeDisplayedValue(this);
+      showGameOver(this);
     }
   });
 });
 
-const reset = document.getElementById('reset_btn')
-
-reset.addEventListener('click', function(){
+resetBtn.addEventListener('click', function(){
   console.log('dsa')
   squares.forEach(square => {
     square.dataset.revealed = 'false'
@@ -36,3 +48,15 @@ reset.addEventListener('click', function(){
   game_over_display.classList.add('hidden');
   game_over_display.classList.remove('alert-danger');
 });
+
+showBtn.addEventListener('click', function(){
+  console.log('dsa')
+  squares.forEach(square => {
+    square.dataset.revealed = 'true'
+    changeDisplayedValue(square)
+  })
+});
+
+
+
+
