482e450c3794da38f43c1949bd5c16fed708ad22
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-24 23:05:49 -0300

    dummy change

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,4 +8,4 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-bundle exec rails db:migrate
+bundle exec rails db:migrate# Dummy change
