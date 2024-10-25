9805279769225d3a247baa23f9f71f593559f126
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-24 21:24:09 -0300

    add gameover and reset

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,18 +2,37 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.querySelectorAll('.boardSquare').forEach(square => {
+const squares = document.querySelectorAll('.boardSquare')
+squares.forEach(square => {
+  const game_over_display = document.getElementById('game_over')
+  let gameLost = false;
   square.addEventListener('click', function() {
+    if (gameLost) return;
     const isRevealed = this.dataset.revealed === 'true';
     if (!isRevealed) {
       this.dataset.revealed = 'true';
       if(this.dataset.bomb === 'true'){
         this.innerHTML = '<span>💣</span>';
+        game_over_display.innerText = 'You Lost.'
+        game_over_display.classList.remove('hidden');
+        game_over_display.classList.add('alert-danger')
       }
       else {
         this.innerHTML = `<span>${this.dataset.attachedBombs}</span>`;
       }
-
     }
   });
+});
+
+const reset = document.getElementById('reset_btn')
+
+reset.addEventListener('click', function(){
+  console.log('dsa')
+  squares.forEach(square => {
+    square.dataset.revealed = 'false'
+    square.innerHTML = '<span>⬜</span>'
+  })
+  const game_over_display = document.getElementById('game_over');
+  game_over_display.classList.add('hidden');
+  game_over_display.classList.remove('alert-danger');
 });
